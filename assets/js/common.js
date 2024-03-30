898f1f175b96bd3ec614dff1dce2ebf387b2d119
ManishKumar-13 <kumarmanish70175@gmail.com>
2024-03-30 23:01:43 +0530

    Initial Commit

---
+++ assets/js/common.js
@@ -0,0 +1,7 @@
+function convertToJSON(response) {
+    return response.json();
+}
+
+document.getElementById('nav-search-button').addEventListener('click', function() {
+    window.location.href = '/search.html';
+})
